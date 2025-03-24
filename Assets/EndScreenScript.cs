f0c14023c38da3948fc0ca4a63a9ab3c1e65befb
VictorAA1116 <156972036+VictorAA1116@users.noreply.github.com>
2025-03-24 17:50:22 -0400

    Added Pause Menu

--- Assets/EndScreenScript.cs
+++ Assets/EndScreenScript.cs
@@ -7,7 +7,7 @@ public class EndScreenScript : MonoBehaviour
 {
     public void BackToMainMenu()
     {
-        SceneManager.LoadSceneAsync(0);
+        SceneManager.LoadScene("MainMenu");
     }
     // Start is called before the first frame update
     void Start()
