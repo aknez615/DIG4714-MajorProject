d1c1fffaf4a4f8b177132d21277c7926e133f187
InfiniumKnight <nasaman1954@gmail.com>
2025-04-02 12:58:14 -0400

    camera set up along with player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,125 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; //For UI stamina bar
+using UnityEngine.UI; //For UI health bar
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float lookSpeedX = 2.0f; //Camera movement for left and right
-    public float lookSpeedY = 2.0f; //Camera movement for up and down
 
-    public float walkSpeed = 3.0f; //Walking speed
-    public float runSpeed = 6.0f; //Running speed
-    private bool isRunning = false;
-    private Vector3 moveDirection;
-    private float cameraVerticalRotation = 0f;
-
-    private float currentStamina;
-    public float stamina = 100f; //Max stamina
-    public float staminaDrain = 10f; //How much stamina will drain per second while running
-    public float staminaRegen = 5f; //Stamina regen per second
-    public float staminaThreshold = 10f; //How much stamina is needed in order for player to run again
-    public Slider staminaBar; //Setting up stamina bar UI
+    public float Speed = 3.0f; //Walking speed
+    private Vector2 move;
 
     public float health = 100f; //Max health
     private float currentHealth; //Current player health
     public Slider healthBar; //Setting up health bar UI
-    public float enemyDamage = 10f; //Setting up enemy damage
 
-    public Transform playerCamera; //Reference to current player camera (Cinemachine Virtual Camera)
-
-    private float rotationX = 0f; //Current vertical rotation of the camera
-
-    private Rigidbody rigidbody;
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+    }
 
     private void Start()
-    {
-        rigidbody = GetComponent<Rigidbody>();
-        currentStamina = stamina; //Initalize player stamina
+    { 
         currentHealth = health; //Initalize player health
     }
 
     private void Update()
     {
-        HandleMovementInput();
-        HandleStamina();
-        UpdateStaminaUI();
-        HandleHealth();
-        UpdateHealthUI();
+        movePlayer();
+        //UpdateHealthUI();
     }
 
     private void FixedUpdate()
     {
-        MovePlayer();
+        movePlayer();
     }
 
-    void HandleMovementInput()
+    void movePlayer()
     {
-        float mouseX = Input.GetAxis("Mouse X") * lookSpeedX;
-        float mouseY = Input.GetAxis("Mouse Y") * lookSpeedY;
+        Vector3 movement = new Vector3(move.x, 0f, move.y);
 
-        transform.Rotate(Vector3.up * mouseX);
+        transform.Translate(movement * Speed * Time.deltaTime, Space.World);
 
-        cameraVerticalRotation -= mouseY;
-        cameraVerticalRotation = Mathf.Clamp(rotationX, -90f, 90f);
-        playerCamera.localRotation = Quaternion.Euler(cameraVerticalRotation, 0f, 0f);
-
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        Vector3 cameraForward = Camera.main.transform.forward;
-        cameraForward.y = 0f;
-        Vector3 cameraRight = Camera.main.transform.right;
-
-        moveDirection = (cameraForward * verticalInput + cameraRight * horizontalInput).normalized;
-
-        isRunning = Input.GetKey(KeyCode.LeftShift) && currentStamina > staminaThreshold;
-
-        if (isRunning)
+        if (movement != Vector3.zero)
         {
-            moveDirection *= runSpeed;
-        }
-
-        else
-        {
-            moveDirection *= walkSpeed;
-        }
-
-        transform.Translate(moveDirection * Time.deltaTime, Space.World);
-    }
-
-    private void MovePlayer()
-    {
-        if (moveDirection.magnitude > 0.1f)
-        {
-            Vector3 movement = moveDirection * Time.fixedDeltaTime;
-            rigidbody.MovePosition(transform.position + movement);
-        }
-    }
-    private void HandleStamina()
-    {
-        if (isRunning)
-        {
-            currentStamina -= staminaDrain * Time.deltaTime;
-        }
-
-        else
-        {
-            currentStamina += staminaRegen * Time.deltaTime;
-        }
-
-        currentStamina = Mathf.Clamp(currentStamina, 0, stamina);
-    }
-
-    private void UpdateStaminaUI()
-    {
-        if (staminaBar != null)
-        {
-            staminaBar.value = currentStamina / stamina; //Update the slider UI with percentage of stamina
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
         }
     }
 
-    private void HandleHealth()
+    private void HandleHealth(float enemyDamage)
     {
         currentHealth -= enemyDamage;
         currentHealth = Mathf.Clamp(currentHealth, 0f, health); //Make sure health doesn't go below zero
